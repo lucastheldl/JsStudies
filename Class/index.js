b8616324bf956e1270836685b36b41000ae40e9c
lucastheldl <lucastheldl@gmail.com>
2023-11-29 15:12:54 -0300

    feat: inheritance

--- Class/index.js
+++ Class/index.js
@@ -14,3 +14,24 @@ const car1 = new Car("Audi", 2009);
 const car2 = new Car("Ferrari", 2015);
 
 console.log(car1, car2);
+
+//________________________
+//Herança
+class Ferrari extends Car {
+  constructor(name, year, model) {
+    super(name, year); //usado para receber os dados da classe pai
+    this.model = model;
+  }
+
+  apresentacao() {
+    console.log(
+      `Este é um carro da marca ${this.name} do modelo ${this.model}`
+    );
+  }
+}
+
+const apresentacaoCarro1 = new Ferrari(
+  "ferrari",
+  "2000",
+  "b2130"
+).apresentacao();
