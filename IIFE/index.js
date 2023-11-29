2b5e7b6205c92c345f0450a31214c8a3735f8831
lucastheldl <lucastheldl@gmail.com>
2023-11-28 21:52:55 -0300

    feat: IIFE

---
+++ IIFE/index.js
@@ -0,0 +1,12 @@
+(function () {
+  //codigo
+  //Escopo limitado a essa função
+  //Função que é chamada automaticamente na inicialização do código
+})();
+
+const resultado = (function () {
+  //codigo
+  //Escopo limitado a essa função
+  const myName = "Lucas";
+  return myName;
+})();
